ec620c62b8a5ccbd079ac75772d33955f84a87ac
alexander de sostoa <alexanderdesostoa@gmail.com>
2020-08-10 18:47:14 -0400

    last commit alex

--- script.js
+++ script.js
@@ -51,25 +51,25 @@ function showPosition(position) {
         var coords = parks.data[i].latLong;
         var latLng = new google.maps.LatLng(parks.data[i].latitude, parks.data[i].longitude);
         var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
+          position: latLng,
+          map: map
         });
 
         var parkImage = $("<img>");
 
-       
+
         parkImage.attr("src", parks.data[i].images[0].url)
 
-       
+
         $("#state-img").append(parkImage)
 
         console.log(parkImage)
 
-    };
+      };
 
     });
   });
-  };
+};
 
 var map;
 
@@ -86,44 +86,28 @@ function initMap(latitude, longitude) {
   const marker = new google.maps.Marker({
     position: uluru,
     map,
-    title: "your location"
+    title: "Your Location"
   });
   const contentString =
-  '<div id="content">' +
-  '<div id="siteNotice">' +
-  "</div>" +
-  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-  '<div id="bodyContent">' +
-  "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-  "sandstone rock formation in the southern part of the " +
-  "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-  "south west of the nearest large town, Alice Springs; 450&#160;km " +
-  "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-  "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-  "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-  "Aboriginal people of the area. It has many springs, waterholes, " +
-  "rock caves and ancient paintings. Uluru is listed as a World " +
-  "Heritage Site.</p>" +
-  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-  "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-  "(last visited June 22, 2009).</p>" +
-  "</div>" +
-  "</div>";
-const infowindow = new google.maps.InfoWindow({
-  content: contentString
-});
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Your location</h1>' 
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
 
-marker.addListener("click", () => {
-  infowindow.open(map, marker);
-});
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
 }
 
 $("#search-btn").on("click", function () {
-     
+
   var userChoice = $("#state :selected").val();
 
-   console.log(userChoice)
+  console.log(userChoice)
 
   initMap()
 
- })
+})
